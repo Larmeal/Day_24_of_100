eaf623c6449ffa5506c7cd9f9647ba9cdbd5a46d
Larmeal <armloveyou252@hotmail.com>
2022-02-10 23:20:25 +0700

    I alreagy resolve old problem this happened long week ago

--- Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -1,12 +1,3 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
 
 with open("/Users/acer/Desktop/Coding/Python Code/100 Days of Code The Complete Python Pro Bootcamp for 2022/Day_24/Mail+Merge+Project+Start/Mail Merge Project Start/Input/Names/invited_names.txt") as invited:
     invite = invited.readlines()
